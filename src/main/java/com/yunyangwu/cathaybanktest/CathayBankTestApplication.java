69cc06dc1f6819f40cd29f8dcb5a5a5eb3e205d6
Oscar <a127309512@gmail.com>
2022-05-30 21:25:11 +0800

    init
    start application completely

---
+++ src/main/java/com/yunyangwu/cathaybanktest/CathayBankTestApplication.java
@@ -0,0 +1,13 @@
+package com.yunyangwu.cathaybanktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathayBankTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CathayBankTestApplication.class, args);
+	}
+
+}
